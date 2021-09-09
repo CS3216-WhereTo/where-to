97a1e27eba3c7f86bef81b0a394c70630196d0b5
teo-jun-xiong <teojunxiong97@gmail.com>
2021-09-10 01:21:16 +0800

    Add location search bars

--- front_end/src/pages/search/SearchHome.jsx
+++ front_end/src/pages/search/SearchHome.jsx
@@ -2,29 +2,67 @@ import { IonPage, IonImg, IonText, IonButton, IonGrid, IonRow, IonCol, IonIcon, 
 import { useEffect, useState } from "react";
 import { withRouter } from "react-router-dom";
 import "./SearchHome.css";
-import Select from 'react-select';import Fuse from "fuse.js";
-
-import Logo from "../../assets/logo.png";
-interface SelectSearchOption {
-  name: string;
-  value: string;
-}
+import Select from "react-select";
+import Fuse from "fuse.js";
+import { ellipseOutline, locateSharp, swapVertical } from "ionicons/icons";
+import { locationSharp } from "ionicons/icons";
 
 const SearchHome: React.FC = (props) => {
-  const options: {label: string, value: string}[] = [
+  const options = [
     { label: "Swedish", value: "sv" },
     { label: "English", value: "en" },
   ];
 
-  
-
   return (
     <IonPage>
-       <Select
-        value={{ label: "Swedish", value: "sv" }}
-        onChange={(e: any)=>console.log(e)}
-        options={options}
-      />
+      <div className="search">
+        <div className="search-container">
+          <div className="start-search">
+            <IonIcon slot="start" icon={ellipseOutline} size="large"></IonIcon>
+            <Select
+              value={options[0]}
+              onChange={(e: any) => console.log(e)}
+              options={options}
+              styles={{
+                container: (provided, state) => ({
+                  ...provided,
+                  display: "flex",
+                  flex: 1,
+                }),
+                control: (provided, state) => ({
+                  ...provided,
+                  display: "flex",
+                  flex: 1,
+                }),
+              }}
+            />
+          </div>
+          <div className="end-search">
+            <IonIcon slot="start" icon={locationSharp} size="large"></IonIcon>
+            <Select
+              value={options[0]}
+              onChange={(e: any) => console.log(e)}
+              options={options}
+              styles={{
+                container: (provided, state) => ({
+                  ...provided,
+                  display: "flex",
+                  flex: 1,
+                }),
+                control: (provided, state) => ({
+                  ...provided,
+                  display: "flex",
+                  flex: 1,
+                }),
+              }}
+            />
+          </div>
+        </div>
+        
+        <div className="swap">
+          <IonIcon slot="start" icon={swapVertical} size="large"></IonIcon>
+        </div>
+      </div>
     </IonPage>
   );
 };
