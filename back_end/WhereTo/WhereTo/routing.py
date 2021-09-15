3e371b9dd6f1047aa3cb490ff500798713be2432
Shuyang <l.shuyang98@gmail.com>
2021-09-16 07:26:45 +0800

    Testing & bug fixes for walk routing, partial implementation of bus routing

--- back_end/WhereTo/WhereTo/routing.py
+++ back_end/WhereTo/WhereTo/routing.py
@@ -21,10 +21,10 @@ bus_route_edges = {}
 def get_walk_route(origin_id, destination_id):
     
     walk_speed = get_walk_speed()
-    time_to_destination, pred = dijkstra_walk(origin_id, destination_id, node_graph, walk_speed)
-    
-    total_duration = time_to_destination
-    total_distance = get_distance_from_walk_duration
+    time_to, pred = dijkstra_walk(origin_id, destination_id, node_graph, walk_speed)
+
+    total_duration = time_to[destination_id]
+    total_distance = get_distance_from_walk_duration(total_duration, walk_speed)
     
     current_id = destination_id
     path = [{"name": node_graph[current_id]["name"], "coordinates": node_graph[current_id]["coordinates"]}]
@@ -47,3 +47,46 @@ def get_walk_route(origin_id, destination_id):
 
     return json.dumps(result)
     
+
+def get_bus_route(origin_id, destination_id):
+    pass
+
+
+def read_test_data():
+    import csv
+    with open("test_data/nodedata.csv") as f:
+        data = csv.reader(f)
+        next(data)
+        for node_id, node_name, node_lat, node_lon, node_type, node_neighbors in data:
+            if node_type == "Bus Stop":
+                node_name = node_name + " Bus Stop"
+            node_id, node_lat, node_lon =  int(node_id), float(node_lat), float(node_lon)
+            neighbors = list(map(lambda x: int(x), node_neighbors.split("/")))
+            curr = {"name":node_name, "type": node_type, "coordinates": (node_lat, node_lon) , "neighbors": neighbors}
+            node_graph[node_id] = curr
+
+    with open("test_data/stopdata.csv") as f:
+        data = csv.reader(f)
+        next(data)
+        for stop_id, stop_name, stop_duration, stop_lat, stop_lon in data:
+            stop_id, stop_duration=  int(stop_id), int(stop_duration)
+            curr = {"stop_duration": stop_duration, "bus_neighbors":[]}
+            bus_stop_graph[stop_id] = curr
+
+    with open("test_data/routedata.csv") as f:
+        data = csv.reader(f)
+        next(data)
+        for start_name, start_id, end_name, end_id, duration, services, route_coordinates in data:
+            start_id, end_id, duration, services, route_coordinates = int(start_id), int(end_id), int(duration), services.split(), route_coordinates.split()
+            plot_coordinates = []
+            for coordinate in route_coordinates:
+                plot_coordinates.append(list(map(lambda x: float(x), coordinate.split("/"))))
+            bus_stop_graph[start_id]["bus_neighbors"].append(end_id)
+            curr = {"duration": duration, "services": services, "polyline": polyline.encode(plot_coordinates)}
+            bus_route_edges[(start_id, end_id)] = curr
+
+read_test_data()
+#print(node_graph)
+#print(bus_stop_graph)
+#print(bus_route_edges)
+print(get_walk_route(100, 616))
--- back_end/WhereTo/WhereTo/routing_helper.py
+++ back_end/WhereTo/WhereTo/routing_helper.py
@@ -38,13 +38,13 @@ def get_walk_speed():
     return DEFAULT_WALK_SPEED
 
 
-def get_bus_arrival_duration(bus_number):
+def get_bus_waiting_time(bus_number, bus_stop_id):
     import random
     return random.randint(1, 10)
 
 
 def dijkstra_walk(origin_id, destination_id, node_graph, walk_speed):
-
+    
     # Check for input validity
     if origin_id == destination_id:
         raise Exception("Error: origin is same as destination")
@@ -53,13 +53,17 @@ def dijkstra_walk(origin_id, destination_id, node_graph, walk_speed):
     if destination_id not in node_graph:
         raise Exception("Error: destination not in graph")
     
-    time_to_destination = None
+    # Time taken to travel to given node
+    time_to = {}
     # Predecessor of given node
     pred = {}
     # Main priority queue for Dijkstra
     pq = minpq()
 
     # Initialize value for Dijkstra algorithm
+    for node_id in node_graph:
+        time_to[node_id] = float("inf")
+    time_to[origin_id] = 0
     pq[origin_id] = 0
 
     # Main loop to compute shortest distance
@@ -76,26 +80,133 @@ def dijkstra_walk(origin_id, destination_id, node_graph, walk_speed):
 
         # Iterate through neighbors and relax edges
         for neighbor_id in node_graph[node_id]['neighbors']:
+
+            if neighbor_id == node_id:
+                continue
+            
             neighbor_coordinates = node_graph[neighbor_id]['coordinates']
             distance_apart = get_distance(node_coordinates, neighbor_coordinates)
-            duration_apart = get_walk_duration_from_distance(distance_apart, walk_speed)
-            new_time_to_neighbor = time_to_node + duration_apart
+            travel_time = get_walk_duration_from_distance(distance_apart, walk_speed)
+            new_time_to_neighbor = time_to_node + travel_time
             
             # Check if relaxation possible
-            if neighbor_id in pq and pq[neighbor_id] <= new_time_to_neighbor:
+            if time_to[neighbor_id] <= new_time_to_neighbor:
+                continue
+            
+            # Update pq directly, ensures no duplicate copies of the same node present in pq
+            time_to[neighbor_id] = new_time_to_neighbor
+            pq[neighbor_id] = new_time_to_neighbor
+            pred[neighbor_id] = node_id
+        
+    if time_to[destination_id] == float("inf"):
+        raise Exception("Error: no path found from origin to destination")
+    
+    return time_to, pred
+
+
+def dijkstra_bus(origin_id, destination_id, node_graph, bus_stop_graph, bus_route_edges, walk_speed):
+
+    # Check for input validity
+    if origin_id == destination_id:
+        raise Exception("Error: origin is same as destination")
+    if origin_id not in node_graph:
+        raise Exception("Error: origin not in graph")
+    if destination_id not in node_graph:
+        raise Exception("Error: destination not in graph")
+
+    # Keep track of bus arrival timings to avoid multiple calls to NextBus API
+    bus_waiting_times = {}
+    
+    target_time_to_destination = None
+    # Main priority queue for Dijkstra
+    pq = minpq()
+
+    # Initialize value for Dijkstra algorithm
+    # Key for pq is (node_id, transport_mode), where transport mode is either "walk" or shuttle bus number (e.g. "A1")
+    pq[(origin_id, "walk")] = 0
+
+    # Main loop to compute shortest distance
+    while len(pq) > 0:
+
+        # Remove item from front of queue
+        (node_id, transport_mode), time_to_node = pq.popitem()
+        node_coordinates = node_graph[node_id]['coordinates']
+        
+        # Check if destination has been reached
+        if node_id == destination_id:
+            target_time_to_destination = time_to_node
+            break
+
+        # Iterate through (walk) neighbors and relax edges
+        for neighbor_id in node_graph[node_id]['neighbors']:
+            neighbor_coordinates = node_graph[neighbor_id]['coordinates']
+            distance_apart = get_distance(node_coordinates, neighbor_coordinates)
+            travel_time = get_walk_duration_from_distance(distance_apart, walk_speed)
+            new_time_to_neighbor = time_to_node + travel_time
+            
+            # Check if relaxation possible
+            if (neighbor_id, "walk") in pq and pq[(neighbor_id, "walk")] <= new_time_to_neighbor:
                 continue
             
             # Update pq directly, ensures no duplicate copies of the same node present in pq
             pq[neighbor_id] = new_time_to_neighbor
-            pred[neighbor_id] = node_id
         
-    if time_to_destination == None:
+        # Check if node is a bus stop
+        if node_id not in bus_stop_graph:
+            continue
+
+        # Iternate through bus neighbors and relax edges
+        for bus_neighbor_id in bus_stop_graph[node_id]["bus_neighbors"]:
+
+            travel_time = bus_route_edges[(node_id, bus_neighbor_id)]["duration"]
+
+            # Iterate through each service that can get from node to bus_neighbor
+            for service in bus_route_edges[(node_id, bus_neighbor_id)]["services"]:
+            
+                # If do not need to change bus
+                if transport_mode == service:
+                    bus_stopping_time = bus_stop_graph[node_id]["stop_duration"]
+                    new_time_to_bus_neighbor = time_to_node + bus_stopping_time + travel_time
+                
+                    # Check if relaxation possible
+                    if (neighbor_id, transport_mode) in pq and pq[(neighbor_id, transport_mode)] <= new_time_to_bus_neighbor:
+                        continue
+
+                    # Update pq directly, ensures no duplicate copies of the same node present in pq
+                    pq[(neighbor_id, transport_mode)] = new_time_to_bus_neighbor
+                
+                # If need to change bus
+                else:
+
+                    # If bus arrival timing has previously been queried
+                    if bus_neighbor_id in bus_waiting_times and service in bus_waiting_times[bus_neighbor_id]:
+                        bus_waiting_time = bus_waiting_times[bus_neighbor_id][service]
+
+                    # If bus arrival timing has not been queried
+                    else:
+                        bus_waiting_time = get_bus_waiting_time(service, bus_neighbor_id)
+                        if bus_neighbor_id not in bus_waiting_times:
+                            bus_waiting_times[bus_neighbor_id] = {}
+                        bus_waiting_times[bus_neighbor_id][service] = bus_waiting_time
+                   
+                    new_time_to_bus_neighbor = time_to_node + bus_waiting_time + travel_time
+
+                    # Check if relaxation possible
+                    if (neighbor_id, service) in pq and pq[(neighbor_id, service)] <= new_time_to_bus_neighbor:
+                        continue
+
+                    # Update pq directly, ensures no duplicate copies of the same node present in pq
+                    pq[(neighbor_id, service)] = new_time_to_bus_neighbor
+    
+    if target_time_to_destination == None:
         raise Exception("Error: no path found from origin to destination")
     
-    return time_to_destination, pred
+    return target_time_to_destination
 
 
-testing_node_graph = {}
+
+
+
 
 
         
