28b645d2afb10d8d7b00971c90e1e136c12cf314
wltan <53135010+wltan@users.noreply.github.com>
2021-09-16 22:53:14 +0800

    Add models for nodes and edges

--- back_end/WhereTo/nodes/models.py
+++ back_end/WhereTo/nodes/models.py
@@ -1,3 +1,41 @@
 from django.db import models
+from django.db.models.deletion import CASCADE
+from django.db.models.fields import IntegerField, TextField, FloatField
+from django.db.models.fields.related import ForeignKey
 
 # Create your models here.
+
+class Node(models.Model):
+    node_id = IntegerField(primary_key=True)
+    name = TextField()
+    latitude = FloatField()
+    longitude = FloatField()
+
+class Landmark(Node):
+    pass
+
+class BusStop(Node):
+    stop_duration = IntegerField()
+
+class Road(Node):
+    pass
+
+class WalkEdge(models.Model):
+    start_id = ForeignKey('Landmark', on_delete=CASCADE, related_name='walk_start')
+    end_id = ForeignKey('BusStop', on_delete=CASCADE, related_name='walk_end')
+
+    class Meta:
+        constraints = [
+            models.UniqueConstraint(fields=['start_id', 'end_id'], name='walk edges are unique')
+        ]
+
+class BusEdge(models.Model):
+    start_id = ForeignKey('Node', on_delete=CASCADE, related_name='bus_start')
+    end_id = ForeignKey('Node', on_delete=CASCADE, related_name='bus_end')
+    duration = IntegerField()
+    polyline = TextField()
+
+    class Meta:
+        constraints = [
+            models.UniqueConstraint(fields=['start_id', 'end_id'], name='bus edges are unique')
+        ]
