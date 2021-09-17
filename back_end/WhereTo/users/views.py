e22c178831f01eec399b9091a1e5320c7e981765
wltan <53135010+wltan@users.noreply.github.com>
2021-09-17 19:26:05 +0800

    Add authentication decorator

--- back_end/WhereTo/users/views.py
+++ back_end/WhereTo/users/views.py
@@ -1,7 +1,32 @@
 from django.http import JsonResponse
+from django.http.response import HttpResponse
 from google.oauth2 import id_token
 from google.auth.transport import requests
+from WhereTo.secrets import GOOGLE_CLIENT_ID
+import json
+
+def authenticated(handler):
+    def wrapped_handler(request):
+        body = json.loads(request.body)
+        token = body['token']
+        try:
+            # Specify the CLIENT_ID of the app that accesses the backend:
+            id_info = id_token.verify_oauth2_token(token, requests.Request(), GOOGLE_CLIENT_ID)
+
+            # ID token is valid. Get the user's Google Account ID from the decoded token.
+            user_id = id_info['sub']
+
+            return wrapped_handler(request, user_id)
+        except ValueError:
+            # Invalid token
+            return HttpResponse(status=401)
+
+    return wrapped_handler
 
 # Create your views here.
-def update_speed(request):
-    return JsonResponse({}, content_type='application/json')
+@authenticated
+def update_speed(request, user_id):
+    body = json.loads(request.body)
+
+    # placeholders to try out authentication
+    return JsonResponse({'client': GOOGLE_CLIENT_ID, 'req': body, 'result': user_id}, content_type='application/json')
