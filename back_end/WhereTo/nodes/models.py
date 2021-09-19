cb2729b387726ef6d200e4f62468f763f1489c5e
Shuyang <l.shuyang98@gmail.com>
2021-09-20 05:29:56 +0800

    Add functions to populate database, changes to nodes and routes model schema

--- back_end/WhereTo/nodes/models.py
+++ back_end/WhereTo/nodes/models.py
@@ -1,19 +1,22 @@
 from django.db import models
+from django.db.models.deletion import CASCADE
 from django.db.models.fields import IntegerField, TextField, FloatField
+from django.db.models.fields.related import OneToOneField
 
 # Create your models here.
 
 class Node(models.Model):
-    node_id = IntegerField(primary_key=True)
+    id = IntegerField(primary_key=True)
     name = TextField()
     lat = FloatField()
     lon = FloatField()
 
-class Landmark(Node):
-    pass
+class Landmark(models.Model):
+    node = OneToOneField(Node, on_delete=CASCADE, primary_key=True)
 
-class BusStop(Node):
+class BusStop(models.Model):
+    node = OneToOneField(Node, on_delete=CASCADE, primary_key=True)
     stop_duration = IntegerField()
 
-class Road(Node):
-    pass
+class Road(models.Model):
+    node = OneToOneField(Node, on_delete=CASCADE, primary_key=True)
