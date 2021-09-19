81d4d3c29d8643dd9423c0d0791e4d6d7752e53b
Shuyang <l.shuyang98@gmail.com>
2021-09-20 03:06:25 +0800

    Move some models from nodes to routes, edit schema to match csv

--- back_end/WhereTo/nodes/models.py
+++ back_end/WhereTo/nodes/models.py
@@ -8,8 +8,8 @@ from django.db.models.fields.related import ForeignKey, OneToOneField
 class Node(models.Model):
     node_id = IntegerField(primary_key=True)
     name = TextField()
-    latitude = FloatField()
-    longitude = FloatField()
+    lat = FloatField()
+    lon = FloatField()
 
 class Landmark(Node):
     pass
@@ -19,27 +19,3 @@ class BusStop(Node):
 
 class Road(Node):
     pass
-
-class WalkEdge(models.Model):
-    start_id = ForeignKey(Node, on_delete=CASCADE, related_name='walk_start')
-    end_id = ForeignKey(Node, on_delete=CASCADE, related_name='walk_end')
-
-    class Meta:
-        constraints = [
-            models.UniqueConstraint(fields=['start_id', 'end_id'], name='walk edges are unique')
-        ]
-
-class BusEdge(models.Model):
-    start_id = ForeignKey(Node, on_delete=CASCADE, related_name='bus_start')
-    end_id = ForeignKey(Node, on_delete=CASCADE, related_name='bus_end')
-    duration = IntegerField()
-    polyline = TextField()
-
-    class Meta:
-        constraints = [
-            models.UniqueConstraint(fields=['start_id', 'end_id'], name='bus edges are unique')
-        ]
-
-class BusService(models.Model):
-    edge_id = OneToOneField(BusEdge, on_delete=CASCADE, primary_key=True)
-    service = CharField(max_length=5)
