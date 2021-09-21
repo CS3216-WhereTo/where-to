b9536506411881d7c4deb3e05b30bb08580d6b8d
wltan <53135010+wltan@users.noreply.github.com>
2021-09-21 14:52:43 +0800

    Add setup script

---
+++ back_end/WhereTo/populate.py
@@ -0,0 +1,9 @@
+from nodes.populate_models import populate_node, populate_busstop, populate_landmark
+from routes.populate_models import populate_busedge, populate_busservice, populate_walkedge
+
+populate_node()
+populate_busstop()
+populate_landmark()
+populate_busedge()
+populate_busservice()
+populate_walkedge()
