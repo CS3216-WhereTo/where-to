36b62f223c84982f230dbf59ea4bcbbf2a2e0a59
wltan <53135010+wltan@users.noreply.github.com>
2021-09-21 15:41:21 +0800

    Fix population script

--- back_end/WhereTo/populate.py
+++ back_end/WhereTo/populate.py
@@ -1,9 +1,10 @@
-from nodes.populate_models import populate_node, populate_busstop, populate_landmark
+from nodes.populate_models import populate_node, populate_busstop, populate_landmark, populate_road
 from routes.populate_models import populate_busedge, populate_busservice, populate_walkedge
 
 populate_node()
 populate_busstop()
 populate_landmark()
+populate_road()
 populate_busedge()
 populate_busservice()
 populate_walkedge()
