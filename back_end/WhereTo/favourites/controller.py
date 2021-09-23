c6907217b5bb246f86220e75f608c42e4c906931
wltan <53135010+wltan@users.noreply.github.com>
2021-09-23 21:56:50 +0800

    Fix id

--- back_end/WhereTo/favourites/controller.py
+++ back_end/WhereTo/favourites/controller.py
@@ -6,7 +6,7 @@ def list_favourites(user):
     node_graph = get_node_graph()
 
     result = []
-    favourites = list(Favourite.objects.filter(user_id=user.id).values_list('node_id', flat=True))
+    favourites = list(Favourite.objects.filter(user_id=user).values_list('node_id', flat=True))
     for node_id in favourites:
         result.append({"node_id": node_id, "name": node_graph[node_id]["name"], "coordinates": node_graph[node_id]["coordinates"]})
     return result
@@ -18,7 +18,7 @@ def add_favourite(user, node_id):
     if node_id not in node_graph or node_graph[node_id]["type"] == "Road":
         return 1
         
-    entry, created = Favourite.objects.get_or_create(user_id=user.id, node_id=node_id)
+    entry, created = Favourite.objects.get_or_create(user_id=user, node_id_id=node_id)
     if not created:
         return 2 # duplicate entry
     
@@ -31,7 +31,7 @@ def remove_favourite(user, node_id):
     if int(node_id) not in node_graph:
         return 1
 
-    num_deleted, details = Favourite.objects.filter(user_id=user.id, node_id=node_id).delete()
+    num_deleted, details = Favourite.objects.filter(user_id=user, node_id_id=node_id).delete()
     if num_deleted == 0:
         return 2 # favourite did not exist
     
