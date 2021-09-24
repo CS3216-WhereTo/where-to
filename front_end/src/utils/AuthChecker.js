c6d5be9db94390957667998cde4342be452cc8d4
teo-jun-xiong <teojunxiong97@gmail.com>
2021-09-24 18:03:51 +0800

    Test offline

--- front_end/src/utils/AuthChecker.js
+++ front_end/src/utils/AuthChecker.js
@@ -1,50 +1,52 @@
 import UserGateway from "../gateways/UserGateway";
 
-const STORAGE_KEY = 'jwtIdToken';
+const STORAGE_KEY = "jwtIdToken";
 
 const gateway = new UserGateway();
 
 const clearLocalToken = () => {
-    localStorage.removeItem(STORAGE_KEY);
-}
+  localStorage.removeItem(STORAGE_KEY);
+};
 
 export async function verifyTokenIfExists() {
-    if (!userTokenExists()) return false;
-    if (!navigator.online) return true;
-    return await gateway.isValidToken();
+  if (!userTokenExists()) return false;
+  if (!navigator.onLine) {
+    return true;
+  }
+  return await gateway.isValidToken();
 }
 
 export default function userTokenExists() {
-    const token = getUserToken();
-    return (token != null);
+  const token = getUserToken();
+  return token != null;
 }
 
 export const getUserToken = () => {
-    return localStorage.getItem(STORAGE_KEY);
-}
+  return localStorage.getItem(STORAGE_KEY);
+};
 
 /**
  * Stores a given encoded user JWT `token` locally.
- * @param {string} token 
+ * @param {string} token
  * @param {function} onSuccess
  */
 export const signUserIn = (token, onSuccess, onFailure, onError) => {
-    localStorage.setItem(STORAGE_KEY, token);
-    return verifyTokenIfExists()
-        .then((valid) => {
-            if (valid) onSuccess();
-            else {
-                clearLocalToken();
-                onFailure();
-            }
-        })
-        .catch((e) => {
-            console.error(e);
-            clearLocalToken();
-            onError(e);
-        });
-}
+  localStorage.setItem(STORAGE_KEY, token);
+  return verifyTokenIfExists()
+    .then((valid) => {
+      if (valid) onSuccess();
+      else {
+        clearLocalToken();
+        onFailure();
+      }
+    })
+    .catch((e) => {
+      console.error(e);
+      clearLocalToken();
+      onError(e);
+    });
+};
 
 export const signUserOut = () => {
-    clearLocalToken();
-}
+  clearLocalToken();
+};
